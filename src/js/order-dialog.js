dc8bdadcfcf27a981ecd57f9f7742f2d80a9b939
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-08-08 23:41:16 +0300

    Добавляет форму заказа

---
+++ src/js/order-dialog.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const BUTTONS = document.querySelectorAll('.js-popup-button');
+const BODY = document.querySelector('.js-body');
+const ORDER_DIALOG = document.querySelector(('.js-order-dialog'));
+const OVERLAY = document.querySelector('.js-modal-overlay');
+const CLOSE_BUTTON = document.querySelector('.js-modal-close');
+
+BUTTONS.forEach((item, i, BUTTONS) => {
+    BUTTONS[i].addEventListener('click', () => {
+        toggleOrderDialog();
+        toggleOverlay();
+        toggleFixBodyScroll();
+    })
+})
+
+CLOSE_BUTTON.addEventListener('click', () => {
+    toggleOrderDialog();
+    toggleOverlay();
+    toggleFixBodyScroll();
+})
+
+function toggleOrderDialog() {
+    ORDER_DIALOG.classList.toggle('closed');
+}
+
+function toggleOverlay() {
+  OVERLAY.classList.toggle('closed');
+}
+
+function toggleFixBodyScroll() {
+    BODY.classList.toggle('modal-active');
+
+    if ( BODY.hasAttribute('style') ) {
+        BODY.removeAttribute('style');
+    } else {
+        BODY.style.paddingRight = getScrollWidth() + 'px';
+    }
+}
+
+function getScrollWidth() {
+    let div = document.createElement('div');
+
+    div.style.overflowY = 'scroll';
+    div.style.width = '50px';
+    div.style.height = '50px';
+
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    return scrollWidth;
+}
