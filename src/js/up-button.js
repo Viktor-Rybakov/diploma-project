c3b7bd82e1416188e6d7167241ad9f4967a6e36a
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-09-19 01:35:13 +0300

    Добавляет кнопку Наверх

---
+++ src/js/up-button.js
@@ -0,0 +1,21 @@
+(function () {
+
+  'use strict';
+
+  const upButton = document.querySelector('.js-up-button');
+
+  window.addEventListener('scroll', () => {
+    if ( window.pageYOffset >= document.documentElement.clientHeight ) {
+      upButton.classList.remove('up-button--hidden');
+    }
+
+    if ( window.pageYOffset < document.documentElement.clientHeight ) {
+      upButton.classList.add('up-button--hidden');
+    }
+  });
+
+  upButton.addEventListener('click', () => {
+    window.scrollTo(0,0);
+  });
+
+}());
